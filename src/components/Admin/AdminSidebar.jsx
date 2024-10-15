872a4a618d78c4075663079153c29fa281e27cca
tejas-limbikai-11 <118275996+tejas-limbikai-11@users.noreply.github.com>
2024-10-16 02:14:51 +0530

    Manage Campaigns UI

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faTachometerAlt, faEnvelope, faHandHoldingHeart, faFileAlt, faUsers, faHome, faInfoCircle, faImage, faProjectDiagram, faCog, faSignOutAlt
 } from '@fortawesome/free-solid-svg-icons'; // Import icons
 
-const SidebarItem = ({ title, icon, children }) => {
+const SidebarItem = ({ title, icon, children, link }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -19,7 +20,13 @@ const SidebarItem = ({ title, icon, children }) => {
       >
         <div className="flex items-center">
           <FontAwesomeIcon icon={icon} className="mr-3" />
-          <span>{title}</span>
+          {link ? (
+            <Link to={link} className="text-white">
+              {title}
+            </Link>
+          ) : (
+            <span>{title}</span>
+          )}
         </div>
         <span>{children && (isOpen ? '▲' : '▼')}</span>
       </div>
@@ -30,54 +37,54 @@ const SidebarItem = ({ title, icon, children }) => {
 
 const Sidebar = () => {
   return (
-    <div className="w-64 bg-[#2A6FF1] h-screen p-6">
+    <div className="w-64 bg-[#2A6FF1] h-screen p-6 overflow-y-auto">
       <div className="text-white text-2xl font-bold mb-10">NGO Admin</div>
 
       {/* Dashboard */}
-      <SidebarItem title="Dashboard" icon={faTachometerAlt} />
+      <SidebarItem title="Dashboard" icon={faTachometerAlt} link="/dashboard" />
 
       {/* Messages */}
-      <SidebarItem title="Messages" icon={faEnvelope} />
+      <SidebarItem title="Messages" icon={faEnvelope} link="/messages" />
 
       {/* NGO Services */}
       <div className="text-gray-400 uppercase py-2 px-4">NGO Services</div>
       <SidebarItem title="Donations" icon={faHandHoldingHeart}>
-        <SidebarItem title="View Donations" />
+        <SidebarItem title="View Donations" link="/donations/view" />
       </SidebarItem>
       <SidebarItem title="Internship" icon={faFileAlt}>
-        <SidebarItem title="Internship Application" />
-        <SidebarItem title="Add Internship Types" />
+        <SidebarItem title="Internship Application" link="/internship/applications" />
+        <SidebarItem title="Add Internship Types" link="/internship/types" />
       </SidebarItem>
       <SidebarItem title="Campaigns" icon={faUsers}>
-        <SidebarItem title="Manage Campaigns" />
+        <SidebarItem title="Manage Campaigns" link="/campaigns/manage" />
       </SidebarItem>
 
       {/* Website Edits */}
       <div className="text-gray-400 uppercase py-2 px-4">Website Edits</div>
       <SidebarItem title="Home Screen" icon={faHome}>
-        <SidebarItem title="Sidebar" />
+        <SidebarItem title="Sidebar" link="/home/sidebar" />
       </SidebarItem>
       <SidebarItem title="About Us" icon={faInfoCircle}>
-        <SidebarItem title="About" />
-        <SidebarItem title="Mission" />
-        <SidebarItem title="Vision" />
-        <SidebarItem title="Impact" />
-        <SidebarItem title="Contact Information" />
-        <SidebarItem title="Manage FAQs" />
+        <SidebarItem title="About" link="/about" />
+        <SidebarItem title="Mission" link="/mission" />
+        <SidebarItem title="Vision" link="/vision" />
+        <SidebarItem title="Impact" link="/impact" />
+        <SidebarItem title="Contact Information" link="/contact" />
+        <SidebarItem title="Manage FAQs" link="/faqs/manage" />
       </SidebarItem>
       <SidebarItem title="Gallery" icon={faImage}>
-        <SidebarItem title="Manage" />
-        <SidebarItem title="Edit Tiles" />
+        <SidebarItem title="Manage" link="/gallery/manage" />
+        <SidebarItem title="Edit Tiles" link="/gallery/edit" />
       </SidebarItem>
       <SidebarItem title="Upcoming Works" icon={faProjectDiagram}>
-        <SidebarItem title="Manage" />
+        <SidebarItem title="Manage" link="/works/manage" />
       </SidebarItem>
 
       {/* Account */}
       <div className="text-gray-400 uppercase py-2 px-4">Account</div>
       <SidebarItem title="Account" icon={faCog}>
-        <SidebarItem title="Settings" />
-        <SidebarItem title="Logout" icon={faSignOutAlt} />
+        <SidebarItem title="Settings" link="/account/settings" />
+        <SidebarItem title="Logout" link="/logout" icon={faSignOutAlt} />
       </SidebarItem>
     </div>
   );
